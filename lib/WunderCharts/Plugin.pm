68f6fdf62d2d46fb1eff789695b70cba20794b0a
Olaf Alders <olaf@wundersolutions.com>
2016-02-19 21:20:39 -0500

    Initial commit.

---
+++ lib/WunderCharts/Plugin.pm
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+package WunderCharts::Plugin;
+
+1;
+
+=pod
+
+=head1 DESCRIPTION
+
+This package contains the plugins which WunderCharts uses to connect to various
+3rd party APIs.
+
+=cut
