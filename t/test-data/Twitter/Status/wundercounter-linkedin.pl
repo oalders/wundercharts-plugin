0f9f986836bd521157d8c0878ecd3106da4cdbbd
Olaf Alders <olaf@wundersolutions.com>
2016-03-20 01:53:00 -0400

    Fix Twitter test data.

--- t/test-data/Twitter/Status/wundercounter-linkedin.pl
+++ t/test-data/Twitter/Status/wundercounter-linkedin.pl
@@ -1,8 +1,7 @@
-{
-    favorite_count => 1910,
+{   favorite_count => 1910,
     id             => 570454045099307008,
     retweet_count  => 2717,
     text =>
         'My own private research shows that a carefully chosen LinkedIn photo can put an immediate stop to recruiter spam.',
-    user => { screen_name => 'wundercounter' } .
+    user => { screen_name => 'wundercounter' },
 }
