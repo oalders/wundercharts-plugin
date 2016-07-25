4780ceb22233fe26f47f51e11b1365b13fd0c178
Olaf Alders <olaf@wundersolutions.com>
2016-07-24 23:48:36 -0400

    Adds sample config file.

---
+++ sample-config.pl
@@ -0,0 +1,9 @@
+{
+    facebook => {
+        access_token    => 'secret_token',
+        consumer_key    => 'secret_consumer_key',
+        consumer_secret => 'secret_consumer_secret',
+        live            => 1,
+    },
+    github => {...},
+}
