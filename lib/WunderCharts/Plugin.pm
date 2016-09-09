e241223040b847583369f181ac73d62723cde1d6
Olaf Alders <olaf@wundersolutions.com>
2016-09-08 22:20:33 -0400

    Updates contributor documentation.

--- lib/WunderCharts/Plugin.pm
+++ lib/WunderCharts/Plugin.pm
@@ -27,4 +27,52 @@ at your disposal.
 A friendly synopsis of the terms under which this software is licensed can be
 found here: L<http://choosealicense.com/licenses/gpl-3.0/>
 
+=head2 LEARN BY EXAMPLE
+
+See L<WunderCharts::Plugin::Twitter> for an example of working with an OAuth app.
+
+See L<WunderCharts::Plugin::Instagram> for an example of working with an OAuth2 app.
+
+See L<WunderCharts::Plugin::HackerNews> for an example of using an API which
+requires no authentication whatsoever.
+
+=head2 REQUIREMENTS
+
+=over
+
+=item Respect the TOS
+
+Plugins may not violate the Terms of Service for the web service in question.
+
+=item Official, Supported APIs
+
+Plugins must only use official, supported APIs.  (No screen scraping).  If you
+want to use an unofficial, but supported API (such as the Hacker News plugin
+does), please open a GitHub issue so that we can discuss this first.
+
+=back
+
+=head1 TESTING
+
+To run the live tests, copy the file C<sample-config.pl> to C<config.pl>.  Add
+the services you'd like to test, with valid OAuth credentials.  This file must
+contain a valid Perl hash as it just gets eval'd by
+C<t/lib/Test/WunderCharts/Plugin.pm>
+
+Currently you may test the following services:
+
+=over
+
+=item facebook
+
+=item github
+
+=item instagram
+
+=item spotify
+
+=item twitter
+
+=back
+
 =cut
--- sample-config.pl
+++ sample-config.pl
@@ -5,5 +5,12 @@
         consumer_secret => 'secret_consumer_secret',
         live            => 1,
     },
-    github => {...},
+    github  => {...},
+    twitter => {
+        access_token        => 'secret_token',
+        access_token_secret => 'secret_access_token',
+        consumer_key        => 'secret_consumer_key',
+        consumer_secret     => 'secret_consumer_secret',
+        live                => 1,
+    }
 }
